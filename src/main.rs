a26259c7b561b144fe5e45dce256de7f564c9c2e
Federico Builes <federico@mheroin.com>
2016-09-05 18:19:41 -0500

    Cleanin up arg matching.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use std::env;
 use std::process;
 
-fn print_help() {
-    println!("print_help");
-}
-
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let argc = args.len();
 
+    if argc == 1 {
+        exit_with_usage();
+    }
+
     match argc {
-        0...1 => {
-            print_help();
-            process::exit(0);
-        }
-        _ => {
-            let command = args[3];
-            let options = args[4];
-    println!("{} {}", command, options);
-            match command {
+        2 => {
+            let command = args[1];
+            println!("{}", command);
+        },
+        3 => {
+            let command = args[1];
+            let options = args[2];
+
+            match &*command {
                 "start" => start(options),
                 "stop" => stop(options),
                 "restart" => restart(options),
                 "info" => info(options),
                 "enabled" => enabled(),
                 "disabled" => disabled(),
+                _ => exit_with_usage(),
             }
 
+        },
+        _ => {
+            exit_with_usage();
         }
     }
-
 }
